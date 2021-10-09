c916172c9e2b35dbbaa1798d6f1705000ed668f4
dangaogit <dangaogit>
2021-10-09 15:31:25 +0800

    All: add templates

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	template, _ := filepath.Abs("templates/index.html")
 	r := gin.Default()
+	r.LoadHTMLFiles(template)
 
 	r.GET("", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello dangaogo.")
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
 	r.Run(":80")
